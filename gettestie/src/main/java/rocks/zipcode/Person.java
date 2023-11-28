a28bee7a17d76d9a17b426a3fc1009844cb8b615
nathan <nathan.guardado@yahoo.com>
2023-11-28 11:40:32 -0500

    Finished

--- gettestie/src/main/java/rocks/zipcode/Person.java
+++ gettestie/src/main/java/rocks/zipcode/Person.java
@@ -1,11 +1,10 @@
-package gettestie.src.main.java.rocks.zipcode;
+package rocks.zipcode;
 
 
 public class Person {
     private String name;
     private int yearOfBirth;
     private Address address;
-
     /**
      * Create a person with given name and age.
      */
@@ -13,54 +12,58 @@ public class Person {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
     }
-
     /**
      * Set a new name for this person.
      */
     public void setName(String newName) {
         name = newName;
     }
-
     /**
      * Return the name of this person.
      */
     public String getName() {
         return name;
     }
-
     /**
      * Set a new birth year for this person.
      */
     public void setYearOfBirth(int newYearOfBirth) {
         yearOfBirth = newYearOfBirth;
     }
-
     /**
      * Return the birth year of this person.
      */
     public int getYearOfBirth() {
         return yearOfBirth;
     }
-
     /**
      * Set a new address for this person.
      */
     public void setAddress(String street, String town, String postCode) {
         address = new Address(street, town, postCode);
     }
-
     /**
      * Return the address of this person.
      */
     public Address getAddress() {
         return address;
     }
-
     /**
      * Return a string representation of this object.
      */
-    public String toString() // redefined from "Object"
+    public String toString()
     {
         return "Name: " + name + "\n" + "Year of birth: " + yearOfBirth + "\n";
+    }
+
+
+    public int compareTo(Person hi) {
+        if(this.name.charAt(0) > hi.name.charAt(0)) {
+            return -1;
+        }
+        else if(this.name.charAt(0) < hi.name.charAt(0)) {
+            return 1;
+        }
+        return 0;
     }
 }
--- gettestie/src/test/java/rocks/zipcode/AppTest.java
+++ gettestie/src/test/java/rocks/zipcode/AppTest.java
@@ -1,9 +1,6 @@
 package rocks.zipcode;
-
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
 /**
  * Unit test for simple App.
  */
